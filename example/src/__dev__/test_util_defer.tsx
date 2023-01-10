5a89227f0d1e092faff1733f2d49971198343fa4
asteriskzuo <zuoyuhsywn@hotmail.com>
2023-01-11 00:37:53 +0800

    perf: useDeferredValue update

--- example/src/__dev__/test_util_defer.tsx
+++ example/src/__dev__/test_util_defer.tsx
@@ -69,19 +69,22 @@ export default function TestUtil() {
   React.useEffect(() => {}, []);
 
   const [value, setValue] = React.useState(0);
+  const [value2, setValue2] = React.useState(0);
 
   const useDeferredValueM = React.useCallback(useDeferredValue, [value]);
 
-  // const timeout = React.useRef<{
-  //   timeoutId: NodeJS.Timeout;
-  //   cur: number;
-  // } | null>(null);
+  type ObjType = {
+    name: string;
+    age: number;
+  };
+
+  const [obj, setObj] = React.useState<ObjType>({ name: 'zs', age: count });
 
   return (
     <View style={{ marginTop: 100 }}>
       <View>
         <RNButton
-          title="noDeferredValue"
+          title="compare value"
           onPress={() => {
             setValue(++count);
           }}
@@ -89,8 +92,31 @@ export default function TestUtil() {
           defer value
         </RNButton>
         <Text>{value}</Text>
-        <Text>{useDeferredValueM(value)}</Text>
-        {/* <Text>{useDeferredValueInternal(value, timeout)}</Text> */}
+        <Text>{useDeferredValue(value)}</Text>
+      </View>
+      <View>
+        <RNButton
+          title="compare value"
+          onPress={() => {
+            setValue2(++count);
+          }}
+        >
+          defer value2
+        </RNButton>
+        <Text>{value2}</Text>
+        <Text>{useDeferredValueM(value2)}</Text>
+      </View>
+      <View>
+        <RNButton
+          title="compare object value"
+          onPress={() => {
+            setObj({ name: obj.name, age: obj.age + 1 });
+          }}
+        >
+          defer object value
+        </RNButton>
+        <Text>{obj.age}</Text>
+        <Text>{useDeferredValueM(obj).age}</Text>
       </View>
     </View>
   );
--- packages/react-native-chat-uikit/src/hooks/index.tsx
+++ packages/react-native-chat-uikit/src/hooks/index.tsx
@@ -21,100 +21,7 @@ export const useAsyncTask = (
   }
 };
 
-// https://juejin.cn/post/7083466010505773093#3
-// This function does not exist before react 18.
-// export const useDeferredValue = (
-//   defer: number,
-//   f: (...args: any[]) => any,
-//   ...args: any[]
-// ) => {
-//   console.log('test:version:', React.version);
-
-//   const timeout = React.useRef<{
-//     timeoutId: NodeJS.Timeout;
-//     cur: number;
-//   }>();
-
-//   const _create = React.useCallback(
-//     (defer: number, f: (...args: any[]) => any, ...args: any[]) => {
-//       if (timeout.current === undefined) {
-//         timeout.current = {
-//           timeoutId: setTimeout(() => f(...args), defer),
-//           cur: new Date().getTime(),
-//         };
-//       }
-//     },
-//     []
-//   );
-//   const _cancel = React.useCallback((defer: number) => {
-//     if (timeout.current) {
-//       if (timeout.current.cur + defer < new Date().getTime()) {
-//         clearTimeout(timeout.current?.timeoutId);
-//         timeout.current = undefined;
-//       }
-//     }
-//   }, []);
-
-//   _cancel(defer);
-//   _create(defer, f, ...args);
-// };
-
 const DEFER = 500;
-export const useDeferredValue2 = <T,>(value: T, defer: number = DEFER) => {
-  const v = versionToArray(React.version);
-  console.warn('test:version:', v);
-  if (v[0] && v[0] >= 18) {
-    throw new Error('Please use the official version.');
-  }
-
-  const _preValue = React.useRef(value);
-  const [_value, setValue] = React.useState(_preValue.current);
-
-  const timeout = React.useRef<{
-    timeoutId: NodeJS.Timeout;
-    cur: number;
-  }>();
-
-  const _create = React.useCallback(
-    (
-      defer: number,
-      dispatch: React.Dispatch<React.SetStateAction<T>>,
-      value: T
-    ) => {
-      if (timeout.current === undefined) {
-        timeout.current = {
-          timeoutId: setTimeout(() => {
-            _preValue.current = value;
-            dispatch(value);
-          }, defer),
-          cur: new Date().getTime(),
-        };
-      }
-    },
-    []
-  );
-  const _cancel = React.useCallback((defer: number) => {
-    console.log('test:useDeferredValue:_cancel:');
-    if (timeout.current) {
-      console.log('test:useDeferredValue:_cancel:1:');
-      const cur = new Date().getTime();
-      if (timeout.current.cur + defer < cur) {
-        console.log(
-          'test:useDeferredValue:_cancel:2:',
-          timeout.current.cur + defer,
-          cur
-        );
-        clearTimeout(timeout.current?.timeoutId);
-        timeout.current = undefined;
-      }
-    }
-  }, []);
-
-  _cancel(defer);
-  _create(defer, setValue, value);
-
-  return _value;
-};
 
 type TimeoutType = {
   timeoutId: NodeJS.Timeout;
@@ -123,21 +30,18 @@ type TimeoutType = {
 
 export const useDeferredValue = <T,>(value: T, defer: number = DEFER) => {
   const v = versionToArray(React.version);
-  console.warn('test:version:', v);
   if (v[0] && v[0] >= 18) {
     throw new Error('Please use the official version.');
   }
 
   const _preValue = React.useRef(value);
   const preValue = React.useMemo(() => {
-    console.log('test:useDeferredValue:useMemo:preValue:', _preValue.current);
     return _preValue;
   }, []);
   const [_value, setValue] = React.useState(preValue.current);
 
   const _timeout = React.useRef<TimeoutType>();
   const timeout = React.useMemo(() => {
-    console.log('test:useDeferredValue:useMemo:timeout:', _timeout.current);
     return _timeout;
   }, []);
 
@@ -149,19 +53,15 @@ export const useDeferredValue = <T,>(value: T, defer: number = DEFER) => {
       value: T,
       preValue: React.MutableRefObject<T>
     ) => {
-      console.log('test:useDeferredValue:_create:');
       if (timeout.current === undefined) {
-        console.log('test:useDeferredValue:_create:1:');
         timeout.current = {
           timeoutId: setTimeout(() => {
-            console.log('test:useDeferredValue:_create:2:', value);
             preValue.current = value;
             timeout.current = undefined;
             dispatch(value);
           }, defer),
           cur: new Date().getTime(),
         };
-        console.log('test:useDeferredValue:_create:3:', timeout.current.cur);
       }
     },
     []
@@ -171,21 +71,10 @@ export const useDeferredValue = <T,>(value: T, defer: number = DEFER) => {
       defer: number,
       timeout: React.MutableRefObject<TimeoutType | undefined>
     ) => {
-      console.log('test:useDeferredValue:_cancel:');
       if (timeout.current) {
         const cur = new Date().getTime();
-        console.log(
-          'test:useDeferredValue:_cancel:1:',
-          timeout.current.cur,
-          cur
-        );
         if (cur <= timeout.current.cur + defer) {
-          console.log(
-            'test:useDeferredValue:_cancel:2:',
-            timeout.current.cur + defer,
-            cur
-          );
-          clearTimeout(timeout.current?.timeoutId);
+          clearTimeout(timeout.current.timeoutId);
           timeout.current = undefined;
         }
       }
