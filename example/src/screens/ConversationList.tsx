cd7fb4faa22670bd0f2bff10bfbca6c81c460e3c
asteriskzuo <zuoyuhsywn@hotmail.com>
2023-01-15 21:10:35 +0800

    chore: update conversation list

--- example/src/screens/ConversationList.tsx
+++ example/src/screens/ConversationList.tsx
@@ -3,6 +3,7 @@ import * as React from 'react';
 import { useWindowDimensions, View } from 'react-native';
 import {
   Badge as UIBadge,
+  Blank,
   createStyleSheet,
   EqualHeightList,
   EqualHeightListItemComponent,
@@ -106,6 +107,7 @@ export default function ConversationListScreen({
   const autoFocus = false;
   const data: ItemDataType[] = [];
   const width = 100;
+  const isEmpty = false;
   const r = COUNTRY.map((value) => {
     const i = value.lastIndexOf(' ');
     const en = value.slice(0, i);
@@ -157,74 +159,81 @@ export default function ConversationListScreen({
       style={{ flex: 1, backgroundColor: theme.colors.background }}
       edges={['right', 'left']}
     >
-      <EqualHeightList
-        onLayout={(event) => {
-          console.log('test:EqualHeightList:', event.nativeEvent.layout.height);
-        }}
-        ref={listRef}
-        items={data}
-        ItemFC={Item}
-        enableAlphabet={enableAlphabet}
-        enableRefresh={enableRefresh}
-        enableHeader={enableHeader}
-        alphabet={{
-          alphabetCurrent: {
-            backgroundColor: 'orange',
-            color: 'white',
-          },
-          alphabetItemContainer: {
-            width: 15,
-            borderRadius: 8,
-          },
-        }}
-        Header={(props) => (
-          <ListSearchHeader
-            autoFocus={autoFocus}
-            onChangeText={(text) => {
-              console.log('test:ListSearchHeader:onChangeText:', Text);
-              queueTask(() => {
-                const r: ItemDataType[] = [];
-                for (const item of data) {
-                  if (item.key.includes(text)) {
-                    r.push(item);
+      {isEmpty ? (
+        <Blank />
+      ) : (
+        <EqualHeightList
+          onLayout={(event) => {
+            console.log(
+              'test:EqualHeightList:',
+              event.nativeEvent.layout.height
+            );
+          }}
+          ref={listRef}
+          items={data}
+          ItemFC={Item}
+          enableAlphabet={enableAlphabet}
+          enableRefresh={enableRefresh}
+          enableHeader={enableHeader}
+          alphabet={{
+            alphabetCurrent: {
+              backgroundColor: 'orange',
+              color: 'white',
+            },
+            alphabetItemContainer: {
+              width: 15,
+              borderRadius: 8,
+            },
+          }}
+          Header={(props) => (
+            <ListSearchHeader
+              autoFocus={autoFocus}
+              onChangeText={(text) => {
+                console.log('test:ListSearchHeader:onChangeText:', Text);
+                queueTask(() => {
+                  const r: ItemDataType[] = [];
+                  for (const item of data) {
+                    if (item.key.includes(text)) {
+                      r.push(item);
+                    }
                   }
-                }
-                listRef.current?.manualRefresh([
-                  {
-                    type: 'clear',
-                  },
-                  {
-                    type: 'add',
-                    data: r,
-                    enableSort: true,
-                  },
-                ]);
-              });
-            }}
-            {...props}
-          />
-        )}
-        ItemSeparatorComponent={ListItemSeparator}
-        onRefresh={(type) => {
-          if (type === 'started') {
-            const en = 'aaa';
-            const v = en + count++;
-            listRef.current?.manualRefresh([
-              {
-                type: 'add',
-                data: [
-                  {
-                    en: v,
-                    ch: v,
-                    key: v,
-                  } as EqualHeightListItemData,
-                ],
-                enableSort: true,
-              },
-            ]);
-          }
-        }}
-      />
+                  listRef.current?.manualRefresh([
+                    {
+                      type: 'clear',
+                    },
+                    {
+                      type: 'add',
+                      data: r,
+                      enableSort: true,
+                    },
+                  ]);
+                });
+              }}
+              {...props}
+            />
+          )}
+          ItemSeparatorComponent={ListItemSeparator}
+          onRefresh={(type) => {
+            if (type === 'started') {
+              const en = 'aaa';
+              const v = en + count++;
+              listRef.current?.manualRefresh([
+                {
+                  type: 'add',
+                  data: [
+                    {
+                      en: v,
+                      ch: v,
+                      key: v,
+                    } as EqualHeightListItemData,
+                  ],
+                  enableSort: true,
+                },
+              ]);
+            }
+          }}
+        />
+      )}
     </SafeAreaView>
   );
 }
