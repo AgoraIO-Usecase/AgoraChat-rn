af1b0f234077a89699fccd28433f2e02dbb05d54
asteriskzuo <zuoyuhsywn@hotmail.com>
2023-04-09 18:07:27 +0800

    chore: update

--- packages/react-native-chat-uikit/src/components/SimulateGif.tsx
+++ packages/react-native-chat-uikit/src/components/SimulateGif.tsx
@@ -19,15 +19,16 @@ export type SimulateGifProps = {
   interval?: number;
 };
 export default function SimulateGif(props: SimulateGifProps): JSX.Element {
-  console.log('SimulateGif:', props);
+  // console.log('SimulateGif:', props);
   const { names, interval } = props;
   const count = names.length;
   const [name, setName] = React.useState<IconName>(names[0] as IconName);
   const index = React.useRef(0);
   React.useEffect(() => {
     const init = () => {
+      // console.log('SimulateGif:init:');
       return setInterval(() => {
-        console.log('SimulateGif:setInterval:');
+        // console.log('SimulateGif:setInterval:');
         setName(names[index.current % count] as IconName);
         index.current += 1;
       }, interval ?? 500);
