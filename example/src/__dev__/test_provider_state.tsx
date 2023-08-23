24b36c0c8530c91b7a59f2ea1bb66428c91b1968
asteriskzuo <zuoyuhsywn@hotmail.com>
2023-08-23 13:53:29 +0800

    example: update

--- example/src/__dev__/test_provider_state.tsx
+++ example/src/__dev__/test_provider_state.tsx
@@ -1,7 +1,8 @@
 import * as React from 'react';
-import { Pressable, View } from 'react-native';
+import { GestureResponderEvent, View } from 'react-native';
 
 export const useForceUpdate = () => {
+  // useState causes component updates.
   const [, updater] = React.useState(0);
   return React.useCallback(() => updater((prev) => prev + 1), []);
 };
@@ -15,19 +16,35 @@ type TestDataContextProps = React.PropsWithChildren<{ data: TestData }>;
 function TestDataContextProvider({ data, children }: TestDataContextProps) {
   console.log('test:TestDataContextProvider:');
   // const updater = React.useRef(useForceUpdate());
-  const [count, setCount] = React.useState(0);
+  const updater = useForceUpdate();
+  // const [count, setCount] = React.useState(0);
   return (
     <TestDataContext.Provider value={data}>
-      <Pressable
-        onPress={() => {
-          console.log('test:TestDataContextProvider:onPress:');
+      <View
+        onResponderRelease={() => {
+          console.log('test:TestDataContextProvider:onResponderRelease:');
           // updater.current?.();
-          setCount(count + 1);
+          // setCount(count + 1);
+          updater();
+        }}
+        onResponderGrant={() => {
+          console.log('test:TestDataContextProvider:onResponderGrant:');
+          // updater.current?.();
+          // setCount(count + 1);
+          updater();
+        }}
+        onStartShouldSetResponder={(_: GestureResponderEvent): boolean => {
+          return false;
+        }}
+        onStartShouldSetResponderCapture={(
+          _: GestureResponderEvent
+        ): boolean => {
+          return false;
         }}
         style={{ height: 200, width: 200, backgroundColor: 'green' }}
       >
         {children}
-      </Pressable>
+      </View>
     </TestDataContext.Provider>
   );
 }
@@ -41,14 +58,27 @@ export function useTestDataContext(): TestData {
 function SubComponent(): JSX.Element {
   const data = useTestDataContext();
   console.log('test:SubComponent:', data);
+  const [count, setCount] = React.useState(0);
   return (
     <View
       style={{ height: 100, width: 100, backgroundColor: 'blue' }}
-      onTouchEnd={() => {
-        console.log('test:SubComponent:onTouchEnd:');
+      onResponderRelease={() => {
+        console.log('test:SubComponent:onResponderRelease:');
         // Does changing the provider context cause redrawing of subcomponents?
         // data.name = 'change';
         data.callback();
+        setCount(count + 1);
+      }}
+      onResponderGrant={() => {
+        console.log('test:SubComponent:onResponderGrant:');
+        data.callback();
+        setCount(count + 1);
+      }}
+      onStartShouldSetResponder={(_: GestureResponderEvent): boolean => {
+        return true;
+      }}
+      onStartShouldSetResponderCapture={(_: GestureResponderEvent): boolean => {
+        return true;
       }}
     />
   );
